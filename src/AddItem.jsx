d3a08a286045086d6393d7bd3fc3d3ddec5bfd27
ssushilgit <shresthasushil741@gmail.com>
2024-10-05 00:35:42 +0545

    useRef hook for shifting foucs towards input text

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {FaPlus } from 'react-icons/fa'
 
 const AddItem = ({newItem, setNewItem, handleSubmit}) => {
+  const inputRef = useRef();
   return (
     <form className='addForm' onSubmit={handleSubmit}>
         <label htmlFor="addItem">Add Item</label>
         <input 
             autoFocus
+            ref={inputRef}
             type="text"
             id='addItem' 
             placeholder='Add Item'
